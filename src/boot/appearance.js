f602202e099459b4bd4ac93ec02a9a20d6d95edb
胡健 <hujian@hujiandeMacBook-Pro.local>
2025-05-19 15:10:08 +0800

    外观apperaance

---
+++ src/boot/appearance.js
@@ -0,0 +1,24 @@
+import { Dark } from 'quasar'
+import { boot } from 'quasar/wrappers'
+import { ConfigDict } from 'src/base/settings'
+
+export default boot(() => {
+    const appearance = localStorage.getItem(ConfigDict.appearance) || "auto";
+
+    switch (appearance) {
+        case "auto":
+            Dark.set(appearance);
+            break;
+        case "true":
+            Dark.set(true);
+            break;
+        case "false":
+            Dark.set(false);
+            break;
+        default:
+            // should not be here
+            Dark.set("auto");
+            break;
+    }
+
+})
